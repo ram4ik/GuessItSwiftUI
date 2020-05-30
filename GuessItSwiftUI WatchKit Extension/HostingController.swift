c0154d9f27bc8d0faeb637ea156ac7e3c5155907
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-05-30 21:55:35 +0300

    Initial Commit

---
+++ GuessItSwiftUI WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  GuessItSwiftUI WatchKit Extension
+//
+//  Created by Ramill Ibragimov on 30.05.2020.
+//  Copyright © 2020 Ramill Ibragimov. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
